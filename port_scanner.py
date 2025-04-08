b027e08bfa6aad222443d9f3586f780191300dd6
Franck-crassava <113307257+Franck-crassava@users.noreply.github.com>
2025-04-08 10:00:50 +0200

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -5,4 +5,12 @@ ports = [22, 80, 443, 3389, 8080]
 
 print(f"Scanning {target}...\n")
 
-
+for port in ports:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
+    result = s.connect_ex((target, port))
+    if result == 0:
+        print(f"Port {port} is open")
+    else:
+        print(f"Port {port} is closed")
+    s.close()
